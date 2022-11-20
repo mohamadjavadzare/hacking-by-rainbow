e646b571d8e0b16e6eb612c9d0058a417f69570d
Mohammad Javad Zare <110285236+mjz589@users.noreply.github.com>
2022-11-20 22:19:21 +0330

    Update Hacking by Rainbow.py

--- Hacking by Rainbow.py
+++ Hacking by Rainbow.py
@@ -1,1 +1,22 @@
+from collections import OrderedDict
+import hashlib
+import csv
 
+def hash_password_hack(input_file_name, output_file_name):
+    with open(input_file_name, 'r') as rfile:
+        with open(output_file_name , 'w' , newline='\n') as wfile:
+            reader = csv.reader(rfile)
+            writer = csv.writer(wfile)
+            dic = OrderedDict()
+            for password in range(1000,10000):
+                temp = str(password)
+                password = str(password).encode('utf-8')
+                h = hashlib.sha256(password)
+                h = h.hexdigest()
+                dic[h] = temp
+            list1 = []
+            for row in reader:
+                username = row.pop(0)
+                password = dic[row[0]]
+                list1 = [username ,password]
+                writer.writerow(list1)
